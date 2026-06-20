206eaa132a8f6fd2ea631e30682d591e44a4f9d6
parhamArdeshiri <parhamardeshiri2021@gmail.com>
2026-06-20 22:40:41 +0330

    Deleted an unnecessary folder

--- backups/csv_factory.py
+++
@@ -1,9 +0,0 @@
-from abstracted.abstracted_factory import AbstractedBaseFactory
-
-from parsers.csv_parser import CSVParser
-
-class CSVParserFactory(AbstractedBaseFactory):
-    
-    def create_parser(self):
-        return CSVParser()
-    
